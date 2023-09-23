6595bf3d88667af7020de9f631c079411531d198
A1cest <Miya_mir@bk.ru>
2023-09-23 16:27:39 +0300

    zadaniye1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Helo World!")
+    println("Heo World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
